c975e98b36d1fb30c1dd286650aeb3761d251825
shivcode22 <136144318+shivcode22@users.noreply.github.com>
2025-04-25 21:11:40 +0530

    Create majority_element.cpp

---
+++ majority_element.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            m[arr[i]]++;
+        }
+        unordered_map<int,int> :: iterator it;
+        for(it = m.begin();it!=m.end();++it){
+            if(((*it).second)>n/2){
+                
+                return ((*it).first);
+            }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        vector<int> a, b;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.majorityElement(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
